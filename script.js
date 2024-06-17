62be6dce398159c45b0cb8f866c7713312c6c26f
deed1996 <26143170+deed1996@users.noreply.github.com>
2024-06-18 00:42:58 +0100

    added html, js and css files

---
+++ script.js
@@ -0,0 +1,17 @@
+let slider = tns({
+    container: ".carousel-container",
+    items: 1,
+    center: true,
+    loop: false,
+    slideBy: "page",
+    swipeAngle: false,
+    speed: 400,
+    controls: false,
+    nav: true,
+    navPosition: "bottom",
+    mouseDrag: !0,
+    controls: false,
+    lazyload: false,
+    autoplay: true,
+    autoplayButtonOutput: !1
+});
